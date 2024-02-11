14a65695e833e8ef44fc951880c3d5e9213456f6
soheilyr <yazdanyar.dev@gmail.com>
2024-02-11 15:58:41 +0330

    dynamic base url in enc

--- src/configs/axiosConfig.tsx
+++ src/configs/axiosConfig.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Fetch = axios.create({
-    baseURL:process.env.BASE_URL
-})
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+});
 
-export default Fetch
+export default Fetch;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,25 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Fetch from "@/configs/axiosConfig";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const sendRequest = async () => {
+  await Fetch.post("/me", {
+    name: "soheil",
+  });
+};
+
 export default function Home() {
-  return <p>Home page</p>;
+  return (
+    <button
+      onClick={() => {
+        Fetch.post("/me", {
+          name: "soheil",
+        });
+      }}
+    >
+      click
+    </button>
+  );
 }
