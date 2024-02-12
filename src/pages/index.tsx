b94a8f9233e99011d87d501518eb689a6c794c6a
soheilyr <yazdanyar.dev@gmail.com>
2024-02-12 14:19:23 +0330

    remove useFetch hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,22 +4,6 @@ import Fetch from "@/configs/axiosConfig";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const sendRequest = async () => {
-  await Fetch.post("/me", {
-    name: "soheil",
-  });
-};
-
 export default function Home() {
-  return (
-    <button
-      onClick={() => {
-        Fetch.post("/me", {
-          name: "soheil",
-        });
-      }}
-    >
-      click
-    </button>
-  );
+  return null;
 }
